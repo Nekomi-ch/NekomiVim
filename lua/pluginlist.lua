18fe079110b9fd1d7455d2f782cc5ce7023a8691
Nekomi Fujiwara <morvenhui@gmail.com>
2025-01-19 01:56:11 +0000

    Some setup

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,8 +1,40 @@
 return {
 	{
+		--comment
   	'numToStr/Comment.nvim',
     config = function()
 			require("Comment").setup()
 		end
 	},
+
+	--theme
+	{
+  	'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+    	require('nordic').load()
+			vim.cmd.colorscheme('nordic')
+    end
+	},
+
+	--lualine
+	{
+	  'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+    	require("lualine").setup({
+      	icons_enabled = true,
+        theme = 'nordic',
+      })
+    end,
+	},
+
+	--language server
+	"williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+
+	"folke/neodev.nvim",
+	
 }
