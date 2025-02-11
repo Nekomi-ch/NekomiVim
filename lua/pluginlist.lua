d3b497a1734ebf92594f7d643f75d95e081f59bb
Nekomi Fujiwara <morvenhui@gmail.com>
2025-02-11 15:21:17 +0000

    LuaLine

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -18,14 +18,44 @@ return {
 		end
 	},
 
+	{
+		'EdenEast/nightfox.nvim',
+		lazy = false,
+    priority = 1000,
+	},
+
 	--lualine
 	{
 	  'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
     	require("lualine").setup({
-      	icons_enabled = true,
-        theme = 'nekonight',
+				options = {
+      		icons_enabled = true,
+        	theme = 'nordfox',
+					section_separators = { left = '', right = '' },
+  				component_separators = { left = '|', right = '|' }
+				},
+				sections = {
+					lualine_a = {'mode'},
+    			lualine_b = {'branch', 'diff', 'diagnostics'},
+    			lualine_c = {'filename'},
+    			lualine_x = {'encoding', 'filetype'},
+    			lualine_y = {'location'},
+    			lualine_z = {
+						function()
+            	return " " .. os.date("%R")
+          	end,
+					},
+				},
+				inactive_sections = {
+    			lualine_a = {},
+    			lualine_b = {},
+    			lualine_c = {'filename'},
+    			lualine_x = {'location'},
+    			lualine_y = {},
+    			lualine_z = {}
+  			},
       })
     end,
 	},
