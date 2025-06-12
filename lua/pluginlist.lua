dc35c9f7d3232e81445d66cc21aa1d5a264d116b
Nekomi Fujiwara <morvenhui@gmail.com>
2025-06-13 00:50:47 +0100

    Added markdown viewer

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -153,6 +153,17 @@ return {
 	--colorizer.lua
 	{
 		'norcalli/nvim-colorizer.lua',
+		config = function()
+			require 'colorizer'.setup()
+		end
+	},
+
+	--markdown preview
+	{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 	},
 
 
