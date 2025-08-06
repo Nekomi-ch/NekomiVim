6691ce36dc1d510547258eb7102ba07891535ca7
Nekomi Fujiwara <morvenhui@gmail.com>
2025-08-06 20:19:14 +0800

    added lazygit

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -178,6 +178,28 @@ return {
     build = function() vim.fn["mkdp#util#install"]() end,
 	},
 
+	--lazygit
+	{
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+	},
+
 	--My own theme test 
 	{
 		dir = "~/FujiIdol.nvim",
